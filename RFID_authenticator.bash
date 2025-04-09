33febe248d0e36c09ad4f23d9b57528814630d0b
Paul <93550092+BennettPaul@users.noreply.github.com>
2025-04-09 11:33:57 -0700

    Update RFID_authenticator.bash

--- RFID_authenticator.bash
+++ RFID_authenticator.bash
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-# Files
 AUTH_FILE="auth.txt"
 LOG_FILE="scan.log"
 SUCCESS_SCRIPT="./success.sh"
 
-# Read input from stdin or a file
-while read -r line; do
-    # Parse line using regex
-    if [[ $line =~ ^([0-9]+)\ Read\ [0-9]+\ bits\ \([0-9]+\ bytes\):\ ([A-F0-9]+)\ :\ ([01]+)$ ]]; then
-        timestamp="${BASH_REMATCH[1]}"
-        hexdata="${BASH_REMATCH[2]}"
-        
-        # Look up hex data in auth file
-        match=$(grep "^$hexdata " "$AUTH_FILE")
-        
-        if [ -n "$match" ]; then
-            user="${match#* }"
-            echo "$timestamp $user SUCCESS" >> "$LOG_FILE"
-            $SUCCESS_SCRIPT "$hexdata" "$user"
-        else
-            echo "$timestamp $hexdata FAILURE" >> "$LOG_FILE"
-        fi
-    else
-        echo "Invalid input format: $line" >&2
-    fi
-done < "${1:-/dev/stdin}"
+# Read the hex value from input
+read -r hexdata
+
+# Get the current Unix timestamp
+timestamp=$(date +%s)
+
+# Look up the hex value in the auth file
+match=$(grep "^$hexdata " "$AUTH_FILE")
+
+if [ -n "$match" ]; then
+    user="${match#* }"
+    echo "$timestamp $user SUCCESS" >> "$LOG_FILE"
+    $SUCCESS_SCRIPT "$hexdata" "$user"
+else
+    echo "$timestamp $hexdata FAILURE" >> "$LOG_FILE"
+fi
